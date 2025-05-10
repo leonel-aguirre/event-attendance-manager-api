8c849a89a8ada4e1cdc0d59dfb7f1dcb12721a4c
Leonel Aguirre <leonel.aguirre182@gmail.com>
2025-05-09 22:22:11 -0600

    🚧 Row insertion through knex working.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const knex = require('knex')
+const knex = require("knex")
+const knexfile = require("./knexfile")
+
+const pg = knex(knexfile.development)
+
+const guests = [
+  { name: "Alice Johnson", email: "alice@example.com" },
+  { name: "Bob Smith", email: "bob@example.com" },
+  { name: "Carlos Díaz", email: "carlos@example.com" },
+]
+
+;(async () => {
+  const result = await pg("guests").insert(guests)
+
+  console.log({ result })
+})()
