1af0de589421215dfe9afd7c03902349e1a4fd7a
Leonel Aguirre <leonel.aguirre182@gmail.com>
2025-05-09 22:10:49 -0600

    🔧 Knex and postgres integration working.

---
+++ migrations/20250510040443_create_invitations_table.js
@@ -0,0 +1,30 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("invitations", function (table) {
+    table.increments("id").primary()
+    table
+      .integer("guest_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("guests")
+      .onDelete("CASCADE")
+    table.string("code").notNullable().unique()
+    table
+      .enu("status", ["pending", "confirmed", "checked_in"])
+      .defaultTo("pending")
+    table.timestamp("checked_in_at").nullable()
+    table.timestamps(true, true)
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("invitations")
+}
