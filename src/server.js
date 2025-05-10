0735732b820989f5f18d71ba049228b264a063d2
Leonel Aguirre <leonel.aguirre182@gmail.com>
2025-05-10 03:30:35 -0600

    💚 Attempt to connect cloud instance to db.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { pg } from './db.js'
 
 import invitationRoutes from './routes/invitations.js'
 
@@ -11,12 +12,21 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/invitations', invitationRoutes)
-
 app.use('/api/ping', (_req, res) => {
   res.json({ message: 'Test after GH actions deploy to Cloud Run' })
 })
 
 const PORT = process.env.PORT || 3000
 
-// eslint-disable-next-line no-console
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+/* eslint-disable no-console */
+;(async () => {
+  try {
+    await pg.migrate.latest()
+    console.log('Migrations are up to date.')
+
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+  } catch (err) {
+    console.error('Migration failed:', err)
+    process.exit(1)
+  }
+})()
